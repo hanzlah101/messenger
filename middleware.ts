56d9aaebe48b8d52198943810457f27cb5f840f3
hanzlah101 <hanzlahjavaid849@gmail.com>
2023-06-04 23:14:35 +0500

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,16 @@
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
+  callbacks: {
+    authorized: async ({ req, token }) => {
+      const pathname = req.nextUrl.pathname;
+
+      if (token) return true;
+
+      return false;
+    },
+  },
+
   pages: {
     signIn: "/",
   },
